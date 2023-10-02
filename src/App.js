7a9f7d1e7c246098ae0c0d8c3f0e2d421ba71de8
KenanAkbarly <kenanekber735@gmail.com>
2023-10-03 00:26:19 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Game from "./components/game";
+import "./components/style.scss"
+
+function App() {
+  return (
+    <div className="">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
